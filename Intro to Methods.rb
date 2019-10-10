38533f18e0bae2f35c5725e2cdd734705434d34e
Mark Kauffman <markkauffman22@gmail.com>
2019-10-10 03:14:45 +0000

    Done.

---
+++ Intro to Methods.rb
@@ -0,0 +1,18 @@
+def greeting
+  puts "Hello World"
+end
+# same with () or w/o when no args.
+ 
+greeting # Executing the method by name
+#=> "Hello World"
+ 
+greeting() # Executing the method again
+#=> "Hello World"
+
+def greeting()
+  puts "Hello World"
+end
+
+
+
+gree
